cd8bae10f6ecea01c7a8fa79d6f43699edeccbf8
CODE-KILLER605 <72912419+CODE-KILLER605@users.noreply.github.com>
2020-10-17 19:23:23 +0400

    Update BOT Project.py

--- BOT Project.py
+++ BOT Project.py
@@ -1,7 +1,12 @@
 import discord
+import os
+import asyncio
+import tracemalloc
 import random
 from discord.ext import commands, tasks
 from itertools import cycle
+
+tracemalloc.start()
 
 client= commands.Bot(command_prefix = "&")
 status = cycle(["Ghilli","Pokkiri","Vettaikaran","Kaavalan","Thuppaki","Katthi","Mersal","Sarkar","Bigil"])
@@ -9,8 +14,26 @@ status = cycle(["Ghilli","Pokkiri","Vettaikaran","Kaavalan","Thuppaki","Katthi",
 @client.event
 async def on_ready():
     changemovies.start()
-    print("good to go")
+    print(f"{client.user} has connected to discord...\n")
+
     #await client.change_presence(status=discord.Status.idle, activity=discord.Game("Ghilli"))
+
+
+class MyContext(commands.Context):
+    async def tick(self, value):
+        # reacts to the message with an emoji
+        # depending on whether value is True or False
+        # if its True, it'll add a green check mark
+        # otherwise, it'll add a red cross mark
+        emoji = '\N{WHITE HEAVY CHECK MARK}' if value else '\N{CROSS MARK}'
+        try:
+            # this will react to the command author's message
+            await self.message.add_reaction(emoji)
+        except discord.HTTPException:
+            # sometimes errors occur during this, for example
+            # maybe you dont have permission to do that
+            # we dont mind, so we can just ignore them
+            pass
 
 #ping command   
 @client.command()
@@ -61,9 +84,40 @@ async def moonji(ctx, user: discord.User = None):
 @tasks.loop(seconds=3)
 async def changemovies():
     await client.change_presence(activity=discord.Game(next(status)))
-    
 
+#wrong commands
+@client.event
+async def on_command_error(ctx , error):
+    await ctx.send(f'Thendi wrong command {ctx.message.author.mention}')
+    await ctx.send(error)
 
-    
-client.run("token")
+'''#guess game
+@client.command()
+async def guess(message, number: int):
+    value = random.randint(1, 6)
+    if number == value:
+        await message.add_reaction(emoji = "✅")
+    else:
+        await message.add_reaction(emoji = "❌")'''
 
+#vijays lines
+@client.command()
+async def punch(ctx,arg):
+    punchs = {
+
+    'ayaan' : 'Nee Pozhapukku Daan Rowdy. Naa Porandadhulendhe Rowdy.',
+    'rounak' : 'Namma Paechu Mattum Daan Silent Ah Irukum, Aana Adi Saravedi.',
+    'ramanan' : 'Oru Vaati Mudivu Panta, Yen Pecha Naane Kekka Maaten.',
+    'harshal' : 'Nee padicha school la na headmaster da.',
+    'ramil' : 'All Arealayum Aiyaa Ghilli Da.',
+    'vivek' : 'I’m Waiting.',
+    'abishai' : 'Saami Munaadi Daan Shaanthama Pesuven. Sakaada Munaadi Ila.',}
+    await ctx.send(punchs.get(arg.lower(),"Taap ana paare <:abeysaale:731486907208433724>"),tts = True)
+
+client.command()
+async def nandri(ctx):
+    if ctx.message.author.id == 6012 :
+        await ctx.send('Puriyuthu')
+        await ctx.bot.logout()
+    else :
+        await ctx.send('panni,do u have common sense')
